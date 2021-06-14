273736cd624c0c10f1426cb23cf053f57a555b3b
Kyle Chivers <chiverskyle@gmail.com>
2021-06-14 16:46:38 -0700

    starting recipe model

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Recipe(models.Model):
+    recipe_name_custom = models.CharField(max_length=400, unique=False, blank=False)
+    recipe_name_title_tag = models.CharField(max_length=400, unique=False, blank=False)
+    recipe_name_h1 = models.CharField(max_length=400, unique=False, blank=False)
+    rating = models.models.DecimalField(max_digits=3, decimal_places=1)
