7d01d1652bda5adc6310e3a7cf8051171cc20067
Kyle Chivers <chiverskyle@gmail.com>
2021-06-17 15:48:56 -0700

    created ingredient models

--- ingredients/models.py
+++ ingredients/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class IngredientCategory(models.Model):
+    category_name = models.CharField(max_length=400, unique=True, blank=False)
+
+class Ingredient(models.Model):
+    ingredient_name = models.CharField(max_length=400, unique=True, blank=False)
+    ingredient_category = models.ManyToManyField(IngredientCategory, blank=True)
