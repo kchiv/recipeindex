3bea8b0b76357e2cb2247c054598d40cd97983f1
Kyle Chivers <chiverskyle@gmail.com>
2021-06-18 16:51:53 -0700

    added bootstrap styling to form

--- recipes/forms.py
+++ recipes/forms.py
@@ -2,6 +2,13 @@ from django.forms import ModelForm
 from . import models
 
 class RecipeForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(RecipeForm, self).__init__(*args, **kwargs)
+        for field in iter(self.fields):
+            self.fields[field].widget.attrs.update({
+                'class': 'form-control'
+        })
+
     class Meta:
         model = models.Recipe
         fields = '__all__'
