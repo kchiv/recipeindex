5448cebab2704c03478b92bc9820691ab767321c
Kyle Chivers <chiverskyle@gmail.com>
2021-06-17 17:10:07 -0700

    registered models

--- ingredients/admin.py
+++ ingredients/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import IngredientCategory, Ingredient
 
 # Register your models here.
+
+admin.site.register(IngredientCategory)
+admin.site.register(Ingredient)
--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import (
+    Author,
+    Publisher,
+    Cuisine,
+    Meal,
+    Dish,
+    Category,
+    Size,
+    Recipe
+)
 
 # Register your models here.
+
+admin.site.register(Author)
+admin.site.register(Publisher)
+admin.site.register(Cuisine)
+admin.site.register(Meal)
+admin.site.register(Dish)
+admin.site.register(Category)
+admin.site.register(Size)
+admin.site.register(Recipe)
