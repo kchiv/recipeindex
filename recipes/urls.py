ceddce1b90c8c6894241359d34c1a5576c21217f
Kyle Chivers <chiverskyle@gmail.com>
2021-06-19 16:49:58 -0700

    need url routing for autocomplete author

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
 app_name = 'recipes'
@@ -6,4 +7,6 @@ app_name = 'recipes'
 urlpatterns = [
     path('recipe-submission/', views.recipe_url_form, name='recipe_url_form'),
     path('form/', views.recipe_full_form, name='recipe_full_form'),
+    url(r'^author-autocomplete/$', views.AuthorAutocomplete.as_view(), name='author-autocomplete',
+    ),
 ]
