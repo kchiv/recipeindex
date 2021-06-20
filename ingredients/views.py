c1c1c0f32d9229e94c91120308c4907e5c5a5990
Kyle Chivers <chiverskyle@gmail.com>
2021-06-19 17:38:36 -0700

    ingredient auto complete view

--- ingredients/views.py
+++ ingredients/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from dal import autocomplete
+from .models import Ingredient
 
 # Create your views here.
+
+class IngredientAutocomplete(autocomplete.Select2QuerySetView):
+    def get_queryset(self):
+        qs = Ingredient.objects.all()
+
+        if self.q:
+            qs = qs.filter(ingredient_name__icontains=self.q)
+        
+        return qs
