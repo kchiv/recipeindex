d9cddeac9c5b51a412a02d7015e557ac1662d53a
Kyle Chivers <chiverskyle@gmail.com>
2021-06-19 17:39:15 -0700

    routing for autocomplete widgets

--- recipes/urls.py
+++ recipes/urls.py
@@ -7,6 +7,10 @@ app_name = 'recipes'
 urlpatterns = [
     path('recipe-submission/', views.recipe_url_form, name='recipe_url_form'),
     path('form/', views.recipe_full_form, name='recipe_full_form'),
-    url(r'^author-autocomplete/$', views.AuthorAutocomplete.as_view(), name='author-autocomplete',
-    ),
+    url(r'^author-autocomplete/$', views.AuthorAutocomplete.as_view(), name='author-autocomplete'),
+    url(r'^publisher-autocomplete/$', views.PublisherAutocomplete.as_view(), name='publisher-autocomplete'),
+    url(r'^cuisine-autocomplete/$', views.CuisineAutocomplete.as_view(), name='cuisine-autocomplete'),
+    url(r'^meal-autocomplete/$', views.MealAutocomplete.as_view(), name='meal-autocomplete'),
+    url(r'^dish-autocomplete/$', views.DishAutocomplete.as_view(), name='dish-autocomplete'),
+    url(r'^category-autocomplete/$', views.CategoryAutocomplete.as_view(), name='category-autocomplete'),
 ]
