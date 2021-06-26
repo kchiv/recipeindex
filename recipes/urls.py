cafe9eebc2cf5182c67b2e1cc425b71f0b30070c
Kyle Chivers <chiverskyle@gmail.com>
2021-06-26 16:29:30 -0700

    routing for recipe detail

--- recipes/urls.py
+++ recipes/urls.py
@@ -7,6 +7,7 @@ app_name = 'recipes'
 urlpatterns = [
     path('recipe-submission/', views.recipe_url_form, name='recipe_url_form'),
     path('form/', views.recipe_full_form, name='recipe_full_form'),
+    path('<int:recipe_id>/', views.recipe_detail, name='recipe_detail'),
     url(r'^author-autocomplete/$', views.AuthorAutocomplete.as_view(create_field='author_name'), name='author-autocomplete'),
     url(r'^publisher-autocomplete/$', views.PublisherAutocomplete.as_view(create_field='publisher_name'), name='publisher-autocomplete'),
     url(r'^cuisine-autocomplete/$', views.CuisineAutocomplete.as_view(create_field='cuisine_name'), name='cuisine-autocomplete'),
