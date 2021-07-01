a079cc69fb68da12b3c51a3e90c75f83303c7363
Kyle Chivers <chiverskyle@gmail.com>
2021-07-01 16:02:43 -0700

    updated to list view for table

--- recipeindex/views.py
+++ recipeindex/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from django.views.generic.base import TemplateView
+from recipes.models import Recipe
 
-class HomePageView(TemplateView):
-
+class HomePageView(ListView):
+    model = Recipe
     template_name = 'template_view/index.html'
