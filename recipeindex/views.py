89e1c37b49cd1ab4d34086f49dc12ddffff00370
Kyle Chivers <chiverskyle@gmail.com>
2021-07-01 16:45:56 -0700

    added class for recipe table

--- recipeindex/views.py
+++ recipeindex/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.views.generic.base import TemplateView
+from django_tables2 import SingleTableView
 from recipes.models import Recipe
+from recipes.tables import RecipeTable
 
-class HomePageView(ListView):
+class HomePageView(SingleTableView):
     model = Recipe
+    table_class = RecipeTable
     template_name = 'template_view/index.html'
