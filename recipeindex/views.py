cd2130a448d950fbe66f7a7e2b69199fc73657be
Kyle Chivers <chiverskyle@gmail.com>
2021-07-01 16:50:05 -0700

    Revert "updated to list view for table"
    This reverts commit a079cc69fb68da12b3c51a3e90c75f83303c7363.

--- recipeindex/views.py
+++ recipeindex/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView
 from django.views.generic.base import TemplateView
-from recipes.models import Recipe
 
-class HomePageView(ListView):
-    model = Recipe
+class HomePageView(TemplateView):
+
     template_name = 'template_view/index.html'
