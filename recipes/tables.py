f9f6f2214a039be71a451da3851a59b01bafb206
Kyle Chivers <chiverskyle@gmail.com>
2021-07-02 15:50:54 -0700

    recipe table fields

--- recipes/tables.py
+++ recipes/tables.py
@@ -3,7 +3,7 @@ from .models import Recipe
 
 class RecipeTable(tables.Table):
     # date = columns.base.Column()
-    date = columns.datetimecolumn.DateTimeColumn(format='SHORT_DATE_FORMAT', short=True)
+    # date = columns.datetimecolumn.DateTimeColumn(format='SHORT_DATE_FORMAT', short=True)
 
 
     def render_date(self, record):
@@ -12,4 +12,4 @@ class RecipeTable(tables.Table):
     class Meta:
         model = Recipe
         template_name = 'django_tables2/bootstrap.html'
-        fields = ('recipe_name_custom', 'recipe_rating', 'recipe_created_date')
+        fields = ('recipe_name_custom', 'recipe_rating', 'recipe_created_date', 'recipe_ingredients')
