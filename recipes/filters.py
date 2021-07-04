5017b51fd9635928a3928684009e14db2f21a896
Kyle Chivers <chiverskyle@gmail.com>
2021-07-04 10:51:01 -0700

    added recipe publisher as filter field

--- recipes/filters.py
+++ recipes/filters.py
@@ -4,7 +4,6 @@ from ingredients.models import Ingredient
 
 class RecipeFilter(django_filters.FilterSet):
     recipe_name_custom = django_filters.CharFilter(lookup_expr='icontains')
-
     recipe_created_date = django_filters.NumberFilter(field_name='recipe_created_date', lookup_expr='year')
     recipe_created_date__gt = django_filters.NumberFilter(field_name='recipe_created_date', lookup_expr='year__gt')
     recipe_created_date__lt = django_filters.NumberFilter(field_name='recipe_created_date', lookup_expr='year__lt')
@@ -23,4 +22,9 @@ class RecipeFilter(django_filters.FilterSet):
 
     class Meta:
         model = Recipe
-        fields = ['recipe_name_custom', 'recipe_created_date', 'recipe_ingredients']
+        fields = [
+            'recipe_name_custom', 
+            'recipe_publisher', 
+            'recipe_created_date', 
+            'recipe_ingredients'
+            ]
