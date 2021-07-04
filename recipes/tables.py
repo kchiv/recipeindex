a8a5030ddea76e241a0820cc2d8abe9693aad77d
Kyle Chivers <chiverskyle@gmail.com>
2021-07-04 12:16:23 -0700

    added rating badges

--- recipes/tables.py
+++ recipes/tables.py
@@ -9,6 +9,7 @@ from .models import Recipe
 class RecipeTable(tables.Table):
     recipe_name_custom = columns.base.Column(verbose_name='Recipe Name')
     recipe_publisher = columns.base.Column(verbose_name='Publisher')
+    recipe_rating = columns.base.Column(verbose_name='Rating')
     recipe_full_ingredients = columns.base.Column(verbose_name='Full Ingredients')
     recipe_created_date = columns.datetimecolumn.DateTimeColumn(format='SHORT_DATE_FORMAT', verbose_name='Created Date')
 
@@ -22,7 +23,15 @@ class RecipeTable(tables.Table):
     def render_recipe_publisher(self, value, record):
         print(value.__dict__)
         return format_html('<a href="?recipe_publisher={}">{}</a>', value.id, value)
-    
+
+    def render_recipe_rating(self, value, record):
+        if value <= 5:
+            return format_html('<span class="badge bg-danger">{}</span>', value)
+        elif value < 8:
+            return format_html('<span class="badge bg-warning text-dark">{}</span>', value)
+        else:
+            return format_html('<span class="badge bg-success">{}</span>', value)
+
     def render_recipe_full_ingredients(self, value, record):
         if value:
             return format_html('<a href="{}#item-d-ingredients"><span style="color: green;"><i class="fas fa-check-circle"></i></span></a>', record.get_absolute_url())
