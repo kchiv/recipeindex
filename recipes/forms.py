487659596f4af79fc7a614658268c438701bc188
Kyle Chivers <chiverskyle@gmail.com>
2021-07-06 10:43:43 -0700

    exclude recipe use from form field

--- recipes/forms.py
+++ recipes/forms.py
@@ -17,6 +17,7 @@ class RecipeForm(forms.ModelForm):
     class Meta:
         model = models.Recipe
         fields = '__all__'
+        exclude = ['recipe_user']
         widgets = {
             'recipe_author': autocomplete.ModelSelect2Multiple(url='recipes:author-autocomplete'),
             'recipe_publisher': autocomplete.ModelSelect2(url='recipes:publisher-autocomplete'),
