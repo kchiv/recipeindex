258e754147cdd1860d0102f34cf0a852a1b11047
Kyle Chivers <chiverskyle@gmail.com>
2021-07-07 13:08:45 -0700

    remove option to create field on publisher object

--- recipes/urls.py
+++ recipes/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('form/', views.recipe_full_form, name='recipe_full_form'),
     path('<int:recipe_id>/', views.recipe_detail, name='recipe_detail'),
     url(r'^author-autocomplete/$', views.AuthorAutocomplete.as_view(create_field='author_name'), name='author-autocomplete'),
-    url(r'^publisher-autocomplete/$', views.PublisherAutocomplete.as_view(create_field='publisher_name'), name='publisher-autocomplete'),
+    url(r'^publisher-autocomplete/$', views.PublisherAutocomplete.as_view(), name='publisher-autocomplete'),
     url(r'^cuisine-autocomplete/$', views.CuisineAutocomplete.as_view(create_field='cuisine_name'), name='cuisine-autocomplete'),
     url(r'^meal-autocomplete/$', views.MealAutocomplete.as_view(create_field='meal_name'), name='meal-autocomplete'),
     url(r'^dish-autocomplete/$', views.DishAutocomplete.as_view(create_field='dish_name'), name='dish-autocomplete'),
