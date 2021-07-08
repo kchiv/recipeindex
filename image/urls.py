a2072ea0dcaabaee9013bdaef88736854922405c
Kyle Chivers <chiverskyle@gmail.com>
2021-07-08 14:40:38 -0700

    set up urls.py for image app

---
+++ image/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from django.conf.urls import url
+from . import views
+
+app_name = 'images'
+
+urlpatterns = [
+    # path('recipe-submission/', views.recipe_url_form, name='recipe_url_form'),
+    # path('form/', views.recipe_full_form, name='recipe_full_form'),
+    # path('<int:recipe_id>/', views.recipe_detail, name='recipe_detail'),
+    # url(r'^author-autocomplete/$', views.AuthorAutocomplete.as_view(create_field='author_name'), name='author-autocomplete'),
+]
