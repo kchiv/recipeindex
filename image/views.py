7423e3666db33ecd24b223d835d4e12f96e9c4c4
Kyle Chivers <chiverskyle@gmail.com>
2021-07-08 14:50:48 -0700

    file autocomplete view

--- image/views.py
+++ image/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from dal import autocomplete
+from .models import ImageFile
 
 # Create your views here.
+
+class FileAutocomplete(autocomplete.Select2QuerySetView):
+    def get_queryset(self):
+        qs = ImageFile.objects.all()
+
+        if self.q:
+            qs = qs.filter(image_file__icontains=self.q)
+        
+        return qs
