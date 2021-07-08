dc2172e9a2975b3c5cd259d4425ba1175d672570
Kyle Chivers <chiverskyle@gmail.com>
2021-07-08 14:50:39 -0700

    autocomplete endpoint for images

--- image/urls.py
+++ image/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     # path('recipe-submission/', views.recipe_url_form, name='recipe_url_form'),
     # path('form/', views.recipe_full_form, name='recipe_full_form'),
     # path('<int:recipe_id>/', views.recipe_detail, name='recipe_detail'),
-    # url(r'^author-autocomplete/$', views.AuthorAutocomplete.as_view(create_field='author_name'), name='author-autocomplete'),
+    url(r'^file-autocomplete/$', views.FileAutocomplete.as_view(), name='file-autocomplete'),
 ]
