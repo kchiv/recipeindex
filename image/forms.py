cd4d2614f9233fe557a0c8295583435cdc79e090
Kyle Chivers <chiverskyle@gmail.com>
2021-07-08 16:54:59 -0700

    removed extraneous code

--- image/forms.py
+++ image/forms.py
@@ -8,21 +8,7 @@ class FileForm(forms.ModelForm):
             self.fields[field].widget.attrs.update({
                 'class': 'form-control'
             })
-        # self.fields['recipe_instantpot'].widget.attrs = {'class': 'form-check-input'}
-        # self.fields['recipe_created_date'].widget.attrs = {'id': 'my_date_picker', 'class': 'form-control'}
 
     class Meta:
         model = models.ImageFile
         fields = '__all__'
-        # exclude = ['recipe_user']
-        # widgets = {
-        #     'recipe_author': autocomplete.ModelSelect2Multiple(url='recipes:author-autocomplete'),
-        #     'recipe_publisher': autocomplete.ModelSelect2Multiple(url='recipes:publisher-autocomplete'),
-        #     'recipe_cuisine': autocomplete.ModelSelect2Multiple(url='recipes:cuisine-autocomplete'),
-        #     'recipe_meal': autocomplete.ModelSelect2Multiple(url='recipes:meal-autocomplete'),
-        #     'recipe_dish': autocomplete.ModelSelect2Multiple(url='recipes:dish-autocomplete'),
-        #     'recipe_category': autocomplete.ModelSelect2Multiple(url='recipes:category-autocomplete'),
-        #     'recipe_event': autocomplete.ModelSelect2Multiple(url='recipes:event-autocomplete'),
-        #     'recipe_ingredients': autocomplete.ModelSelect2Multiple(url='ingredients:ingredient-autocomplete'),
-        #     'recipe_file_storage': AddAnotherWidgetWrapper(autocomplete.ModelSelect2Multiple(url='images:file-autocomplete'), reverse_lazy('recipes:recipe_url_form'),),
-        # }
