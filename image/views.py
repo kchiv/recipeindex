f95852efa0396176c238d91c61ead1ba1aea2183
Kyle Chivers <chiverskyle@gmail.com>
2021-07-08 16:15:49 -0700

    create view for files

--- image/views.py
+++ image/views.py
@@ -1,8 +1,20 @@
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
 from django.shortcuts import render
 from dal import autocomplete
 from .models import ImageFile
+from .forms import FileForm
+from django.views.generic.edit import CreateView
 
 # Create your views here.
+
+class FileCreate(CreateView):
+    model = ImageFile
+    template_name = 'image/file_form.html'
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse('images:file_detail', kwargs={'pk': self.object.pk})
 
 class FileAutocomplete(autocomplete.Select2QuerySetView):
     def get_queryset(self):
