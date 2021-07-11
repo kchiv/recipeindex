39fe71b404a9ea9315af13e9e658fbb9bc8e2ac5
Kyle Chivers <chiverskyle@gmail.com>
2021-07-11 13:29:46 -0700

    added routing for edit and delete views

--- image/urls.py
+++ image/urls.py
@@ -7,6 +7,8 @@ app_name = 'images'
 urlpatterns = [
     # path('recipe-submission/', views.recipe_url_form, name='recipe_url_form'),
     path('form/', views.FileCreate.as_view(), name='file_form'),
+    path('form/edit/<int:file_id>/', views.FileEdit.as_view(), name='file_form_edit'),
+    path('form/delete/<int:file_id>/', views.FileDelete.as_view(), name='file_delete'),
     path('<int:file_id>/', views.file_detail, name='file_detail'),
     url(r'^file-autocomplete/$', views.FileAutocomplete.as_view(), name='file-autocomplete'),
 ]
