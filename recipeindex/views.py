bc87a649905bfc488b4abafe17f15d4ce89e6276
Kyle Chivers <chiverskyle@gmail.com>
2021-07-11 09:43:11 -0700

    added table view for publisher

--- recipeindex/views.py
+++ recipeindex/views.py
@@ -3,16 +3,24 @@ from django.views.generic import ListView
 from django.views.generic.base import TemplateView
 from django_filters.views import FilterView
 from django_tables2 import SingleTableView
-from recipes.models import Recipe
-from recipes.tables import RecipeTable
+from recipes.models import Recipe, Publisher
+from recipes.tables import RecipeTable, PublisherTable
 from recipes.filters import RecipeFilter
 from django_tables2.views import SingleTableMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-class HomePageView(LoginRequiredMixin, SingleTableMixin, FilterView):
+class RecipeView(LoginRequiredMixin, SingleTableMixin, FilterView):
     model = Recipe
     table_class = RecipeTable
     template_name = 'template_view/index.html'
     filterset_class = RecipeFilter
+    login_url = '/admin/'
+    redirect_field_name = 'home'
+
+class PublisherView(LoginRequiredMixin, SingleTableMixin, FilterView):
+    model = Publisher
+    table_class = PublisherTable
+    template_name = 'template_view/index.html'
+    # filterset_class = RecipeFilter
     login_url = '/admin/'
     redirect_field_name = 'home'
