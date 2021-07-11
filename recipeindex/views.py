173499ac4376a769d3b855b36efa52d518480638
Kyle Chivers <chiverskyle@gmail.com>
2021-07-11 11:28:19 -0700

    file table view

--- recipeindex/views.py
+++ recipeindex/views.py
@@ -4,8 +4,11 @@ from django.views.generic.base import TemplateView
 from django_filters.views import FilterView
 from django_tables2 import SingleTableView
 from recipes.models import Recipe, Publisher
+from image.models import ImageFile
 from recipes.tables import RecipeTable, PublisherTable
+from image.tables import FileTable
 from recipes.filters import RecipeFilter
+from image.filters import FileFilter
 from django_tables2.views import SingleTableMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -22,5 +25,13 @@ class PublisherView(LoginRequiredMixin, SingleTableMixin, FilterView):
     table_class = PublisherTable
     template_name = 'template_view/index.html'
     # filterset_class = RecipeFilter
+    login_url = '/admin/'
+    redirect_field_name = 'home'
+
+class FileView(LoginRequiredMixin, SingleTableMixin, FilterView):
+    model = ImageFile
+    table_class = FileTable
+    template_name = 'template_view/index.html'
+    filterset_class = FileFilter
     login_url = '/admin/'
     redirect_field_name = 'home'
