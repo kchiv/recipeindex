56f67260d10f3dde65dad1ecd3fbcb6ed122a7ea
Kyle Chivers <chiverskyle@gmail.com>
2021-07-11 13:29:35 -0700

    updated edit file in table

--- image/tables.py
+++ image/tables.py
@@ -4,7 +4,18 @@ from django_tables2.utils import A
 from .models import ImageFile
 
 class FileTable(tables.Table):
+    file_edit = columns.TemplateColumn(
+        '<a class="btn btn-secondary" href="{% url \'images:file_form_edit\' record.id %}"><i class="fas fa-edit"></i></a>', 
+        verbose_name='Edit', 
+        orderable=False)
 
     class Meta:
         model = ImageFile
         template_name = 'django_tables2/bootstrap-responsive.html'
+        fields = (
+            'id',
+            'image_name',
+            'file_edit',
+            'image_file',
+            'publication_date',
+        )
