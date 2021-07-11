fa6ebccdbc33a89bc84ebdddd6e44e48bc82ef00
Kyle Chivers <chiverskyle@gmail.com>
2021-07-11 13:29:53 -0700

    edit and delete file views

--- image/views.py
+++ image/views.py
@@ -1,6 +1,6 @@
 from django.urls import reverse
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django_addanother.views import CreatePopupMixin
 from dal import autocomplete
 from .models import ImageFile
@@ -15,6 +15,31 @@ class FileCreate(CreatePopupMixin, CreateView):
 
     def get_success_url(self):
         return reverse('images:file_detail', kwargs={'file_id': self.object.pk})
+
+class FileEdit(UpdateView):
+    model = ImageFile
+    form_class = FileForm
+    template_name = 'image/file_form.html'
+
+    def get_object(self):
+        return ImageFile.objects.get(pk=self.kwargs['file_id'])
+
+    def get_success_url(self):
+        return reverse('file_table')
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['edit'] = True
+        return context
+
+class FileDelete(DeleteView):
+    model = ImageFile
+
+    def get_object(self):
+        return ImageFile.objects.get(pk=self.kwargs['file_id'])
+
+    def get_success_url(self):
+        return reverse('file_table')
 
 def file_detail(request, file_id):
     file_obj = get_object_or_404(ImageFile, pk=file_id)
