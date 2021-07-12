cb374f689b90f2b5e9d2e5bd3b13b8da473a1aa2
Kyle Chivers <chiverskyle@gmail.com>
2021-07-11 17:01:33 -0700

    added allowed hosts

--- recipeindex/settings.py
+++ recipeindex/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-s*r2%p8$bmyyfw_cn$mqm-*+hx%cb**q^agdl6s(jq+e$w0!3q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','recipeindex.herokuapp.com']
 
 
 # Application definition
