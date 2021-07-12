94914666ed673c36c0b17e68ddf1fa3e51a7391b
Kyle Chivers <chiverskyle@gmail.com>
2021-07-12 14:24:15 -0700

    limit ingredient autocomplete to user owned

--- ingredients/views.py
+++ ingredients/views.py
@@ -6,9 +6,12 @@ from .models import Ingredient
 
 class IngredientAutocomplete(autocomplete.Select2QuerySetView):
     def get_queryset(self):
-        qs = Ingredient.objects.all()
+        qs = Ingredient.objects.filter(ingredient_user=self.request.user)
 
         if self.q:
             qs = qs.filter(ingredient_name__icontains=self.q)
         
         return qs
+    
+    def create_object(self, text):
+        return self.get_queryset().create(**{self.create_field: text, 'ingredient_user': self.request.user})
