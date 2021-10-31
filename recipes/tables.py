6af3679707295e4ca0d597ce53a0f3bcc0116ba8
Kyle Chivers <chiverskyle@gmail.com>
2021-10-31 12:27:38 -0700

    removed button to fix styling issues in other browsers

--- recipes/tables.py
+++ recipes/tables.py
@@ -126,14 +126,14 @@ class RecipeTable(tables.Table):
 
     def render_recipe_name_custom(self, value, record):
         return format_html('''
-        <a href="{}" type="button" class="btn btn-secondary" data-bs-toggle="tooltip" data-bs-html="true" title="<span><strong>Title:</strong> <em>{}</em></span><br><span><strong>H1:</strong> <em>{}</em></span>">
+        <a href="{}" class="btn btn-secondary" data-bs-toggle="tooltip" data-bs-html="true" title="<span><strong>Title:</strong> <em>{}</em></span><br><span><strong>H1:</strong> <em>{}</em></span>">
             {}
         </a>
         ''', record.get_absolute_url(), record.recipe_name_title_tag, record.recipe_name_h1, value.title())
     
     def render_recipe_url(self, value):
         return format_html('''
-        <a href="{}" type="button" class="btn btn-primary" data-bs-toggle="tooltip" data-bs-html="true" title="<span><em>{}</em></span>">
+        <a href="{}" class="btn btn-primary" data-bs-toggle="tooltip" data-bs-html="true" title="<span><em>{}</em></span>">
             <i class="fas fa-external-link-alt"></i>
         </a>
         ''', value, value)
